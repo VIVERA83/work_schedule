6bfe6d8e1765ee7797b45f907d54d277edc8f247
vivera83 <vivera83@yandex.ru>
2025-01-12 14:41:04 +0300

    исправлена ошибка с некорректным уделение не используемых работников

--- work_schedule/store/scheduler/combined_employees_work_plan.py
+++ work_schedule/store/scheduler/combined_employees_work_plan.py
@@ -1,8 +1,11 @@
 from collections import defaultdict
+from datetime import datetime
+
+from icecream import ic
 
 from work_schedule.store.scheduler.employee_work_plan import EmployeeWorkPlan
 from work_schedule.store.scheduler.utils import DATE, SIGN, SIGNAL_WORK, SIGNAL_WEEKEND
-
+ic.includeContext = True
 
 class CombinedEmployeesWorkPlan:
     __employee_work_plan: dict[DATE, SIGN]
@@ -11,7 +14,7 @@ class CombinedEmployeesWorkPlan:
     def __init__(self, employee_1: EmployeeWorkPlan, employee_2: EmployeeWorkPlan):
         self.employee_1 = employee_1
         self.employee_2 = employee_2
-        self.__merge_employee_work_plan()
+        self.__create_employee_work_plan()
 
     def get_employee_work_plan(self) -> dict[DATE, SIGN]:
         """Возвращает план работы сотрудников."""
@@ -21,13 +24,12 @@ class CombinedEmployeesWorkPlan:
         """Возвращает сотрудников, которые не были задействованы в работе."""
         return self.__unused_employees
 
-    def __merge_employee_work_plan(self):
+    def __create_employee_work_plan(self):
         self.__employee_work_plan = defaultdict(dict)
         self.__merge_unused_employees()
 
         for (date_1, name_1), (_, name_2) in zip(self.employee_1.get_employee_work_plan().items(),
                                                  self.employee_2.get_employee_work_plan().items()):
-
             temp = {self.employee_1.name: name_1}
             if name_1 != name_2:
                 if name_1 in [SIGNAL_WEEKEND]:
@@ -42,7 +44,7 @@ class CombinedEmployeesWorkPlan:
                 if name_1 in [SIGNAL_WEEKEND] and name_2 in [SIGNAL_WEEKEND]:
                     temp.update({self.employee_2.name: name_2})
                 else:
-                    if data := list(self.__unused_employees.pop(date_1, {}).keys()):
+                    if data := list(self.__unused_employees.get(date_1, {}).keys()):
                         temp.update({self.employee_2.name: data[-1]})
                     else:
                         temp.update({self.employee_2.name: SIGNAL_WORK})
@@ -50,6 +52,8 @@ class CombinedEmployeesWorkPlan:
 
             # удаление повторов
             self.__removing_duplicates_from_unused(date_1)
+            # if date_1 == "13-01-2025":
+            #     break
 
     def __merge_unused_employees(self):
         self.__unused_employees = {
@@ -64,15 +68,42 @@ class CombinedEmployeesWorkPlan:
         if data := self.__unused_employees.get(date):
             a = set(self.__employee_work_plan[date].values())
             b = set(data.keys())
-            if c := b - a:
-                self.__unused_employees[date] = {
-                    element: self.__unused_employees[date][element]
-                    for element in c
-                }
-            elif a == b:
-                self.__unused_employees[date] = {
-                    element: self.__unused_employees[date][element]
-                    for element in c
-                }
+            for element in a:
+                if element in b:
+                    del data[element]
             if not self.__unused_employees[date]:
                 del self.__unused_employees[date]
+            # a = set(self.__employee_work_plan[date].values())
+            # b = set(data.keys())
+            # ic(date,a,b,b-a, a-b )
+            # if c := b - a:
+            #     self.__unused_employees[date] = {
+            #         element: self.__unused_employees[date][element]
+            #         for element in c
+            #     }
+            # elif a == b:
+            #     self.__unused_employees[date] = {
+            #         element: self.__unused_employees[date][element]
+            #         for element in c
+            #     }
+            # if not self.__unused_employees[date]:
+            #     del self.__unused_employees[date]
+
+    def set_date(self, start: datetime, end: datetime) -> None:
+        """Устанавливает период расчета графика."""
+
+        self.employee_1.set_date(start, end)
+        self.employee_2.set_date(start, end)
+        self.__create_employee_work_plan()
+
+    def add_combined_employees_work_plan(self, employee: "CombinedEmployeesWorkPlan" = None) -> None:
+        """Добавляет к текущему объекту новый объект CombinedEmployeesWorkPlan."""
+        temp = {}
+        for date, plan in self.get_employee_work_plan().items():
+            t = {}
+            for name, status in plan.items():
+                if status == SIGNAL_WORK:
+                    t[name] = SIGNAL_WORK
+            if t:
+                temp[date] = t
+        return temp, self.get_unused_employees()
