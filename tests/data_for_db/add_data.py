15280c8b3983ba0a92a86a1b514016463b9a40cd
vivera83 <vivera83@yandex.ru>
2025-02-14 22:47:47 +0300

    обновлен модуль добавления в БД тестовых данных из csv файлов 1. добавлены данные для таблицы car_schedule_history - car_schedule_history.csv 2. добавлены данные для таблицы work_schedule_history - work_schedule_history.csv 3. создан класс который добавляет данные в БД по списку файлов.

--- tests/data_for_db/add_data.py
+++ tests/data_for_db/add_data.py
@@ -1,6 +1,4 @@
-import logging
 from logging import Logger
-
 import psycopg2
 import csv
 from dotenv import load_dotenv
@@ -8,71 +6,77 @@ from dotenv import load_dotenv
 from core.logger import setup_logging
 from core.settings import PostgresSettings
 
-load_dotenv()
+PATH_TO_CSV = str
+TABLE_NAME = str
 
 
-def add_data_to_db_table(
-    path_to_csv: str, table_name: str, conn: psycopg2.extensions.connection
-):
-    """
-    Функция для добавления данных из csv файла в таблицу БД.
+class ExportData:
+    """Класс для добавления данных в БД."""
 
-    :param path_to_csv: Путь к csv файлу
-    :param table_name: Название таблицы в БД
-    :param conn: Подключение к БД
-    :return:
-    """
-    cur = conn.cursor()
-    with open(path_to_csv, "r") as f:
-        reader = csv.reader(f)
-        columns = next(reader)
-        count_columns = ",".join(["%s"] * len(columns))
-        columns = ",".join(columns)
-        for row in reader:
-            cur.execute(
-                f"INSERT INTO {pg_settings.postgres_schema}.{table_name} ({columns}) VALUES ({count_columns})",
-                row,
-            )
+    def __init__(self, pg_settings: PostgresSettings, logger: Logger = Logger):
+        """Конструктор класса.
 
-    conn.commit()
+        :param pg_settings: Настройки подключения к БД
+        :param logger: Логгер
+        """
+        self.logger = logger
+        self.pg_settings = pg_settings
+        self.conn = psycopg2.connect(
+            f"dbname={pg_settings.postgres_db} "
+            f"user={pg_settings.postgres_user} "
+            f"password={pg_settings.postgres_password.get_secret_value()} "
+            f"host={pg_settings.postgres_host} "
+            f"port={pg_settings.postgres_port}"
+        )
 
+    def add_data_to_db_table(self, path_to_csv: str, table_name: str):
+        """Функция для добавления данных из csv файла в таблицу БД.
 
-def add_data_to_db(
-    conn: psycopg2.extensions.connection,
-    files: list[tuple[str, str]],
-    logger: logging.Logger = Logger,
-):
-    """Функция для добавления данных из csv файла в таблицы БД.
+        :param path_to_csv: Путь к csv файлу
+        :param table_name: Название таблицы в БД
+        :return:
+        """
+        cur = self.conn.cursor()
+        with open(path_to_csv, "r") as f:
+            reader = csv.reader(f)
+            columns = next(reader)
+            count_columns = ",".join(["%s"] * len(columns))
+            columns = ",".join(columns)
+            for row in reader:
+                cur.execute(
+                    f"INSERT INTO {self.pg_settings.postgres_schema}.{table_name} ({columns}) VALUES ({count_columns})",
+                    row,
+                )
 
-    :param conn: Подключение к БД
-    :param files: Список кортежей из пути к файлу и названия таблицы
-    :param logger: Логгер
-    :return:
-    """
+        self.conn.commit()
 
-    for path, table in files:
-        try:
-            add_data_to_db_table(path, table, conn)
-            logger.info(f"Данные из файла {path} добавлены в таблицу {table}")
-        except Exception as e:
-            connection.rollback()
-            logger.error(
-                f"Ошибка при добавлении данных в таблицу {table} из файла {path}"
-            )
-            logger.error(e)
+    def add_data_to_db(self, files: list[tuple[PATH_TO_CSV, TABLE_NAME]]):
+        """Функция для добавления данных из csv файла в таблицы БД.
+
+        :param files: Список кортежей из пути к файлу и названия таблицы
+        :return:
+        """
+
+        for path, table in files:
+            try:
+                self.add_data_to_db_table(path, table)
+                self.logger.info(f"Данные из файла {path} добавлены в таблицу {table}")
+            except Exception as e:
+                self.conn.rollback()
+                self.logger.error(
+                    f"Ошибка при добавлении данных в таблицу {table} из файла {path}"
+                )
+                self.logger.error(e)
+
+    def close(self):
+        """Закрытие подключения к БД."""
+
+        self.logger.info("Закрытие подключения к БД")
+        self.conn.close()
 
 
 if __name__ == "__main__":
-    logger = setup_logging()
-    pg_settings = PostgresSettings()  # noqa
-
-    connection = psycopg2.connect(
-        f"dbname={pg_settings.postgres_db} "
-        f"user={pg_settings.postgres_user} "
-        f"password={pg_settings.postgres_password.get_secret_value()} "
-        f"host={pg_settings.postgres_host} "
-        f"port={pg_settings.postgres_port}"
-    )
+    load_dotenv()
     data = [
         ("data/schedule_types.csv", "schedule_types"),
         ("data/car.csv", "car"),
@@ -80,6 +84,8 @@ if __name__ == "__main__":
         ("data/crew.csv", "crew"),
         ("data/crew_cars.csv", "crew_cars"),
         ("data/crew_drivers.csv", "crew_drivers"),
+        ("data/work_schedule_history.csv", "work_schedule_history"),
+        ("data/car_schedule_history.csv", "car_schedule_history"),
     ]
-    add_data_to_db(connection, data, logger)
-    connection.close()
+    export_data = ExportData(PostgresSettings(), setup_logging())  # noqa
+    export_data.add_data_to_db(data)
