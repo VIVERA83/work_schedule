1ed402c616196820dfb146fd0575aa4575e1142b
vivera83 <vivera83@yandex.ru>
2025-02-15 20:07:16 +0300

    удалили не используемые файлы

--- test_area/re_.py
+++
@@ -1,8 +0,0 @@
-import re
-
-pattern = r"\((.*?)\)"
-
-string = ':  Key (driver_id)=(0) is not present in table "driver".'
-matches = re.findall(pattern, string)
-
-print(matches)  # Output: ['driver_id', '0']
