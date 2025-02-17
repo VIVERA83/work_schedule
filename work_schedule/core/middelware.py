1ebe2e4ee8d612ebbf636d87fc63694213a267db
vivera83 <vivera83@yandex.ru>
2025-02-17 22:29:10 +0300

    разкоментирован вывод traceback - исключений

--- work_schedule/core/middelware.py
+++ work_schedule/core/middelware.py
@@ -27,7 +27,7 @@ class ErrorHandlingMiddleware(BaseHTTPMiddleware):
             response = await call_next(request)
             return response
         except Exception as error:
-            # traceback.print_exc()
+            traceback.print_exc()
             await save_log_file("log.txt", traceback.format_exc(), f"{request.client.host} {request.method} {request.url}")
             code = getattr(error, "code", status.HTTP_500_INTERNAL_SERVER_ERROR)
             return JSONResponse(
