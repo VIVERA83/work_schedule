d2c984f4c07bad39beabcc2857ee5da75abb128d
vivera83 <vivera83@yandex.ru>
2025-02-20 11:35:40 +0300

    убраны не нужные файлы и закоментированный код

--- work_schedule/api/manager/views.py
+++ work_schedule/api/manager/views.py
@@ -31,15 +31,6 @@ class ManagerViews(BaseView):
                 "summary": "Добавить машину",
                 "description": "Добавить машину и назначения графика работы (проведения `ППО/ППР`).",
             },
-            # "create_assign_car_driver": {
-            #     "path": "/assign_car_driver",
-            #     "methods": ["POST"],
-            #     "annotations": {"data": AssignCarDriverCreateSchema},
-            #     "response_model": AssignCarDriverSchema,
-            #     "summary": "Закрепить водителя за машиной",
-            #     "description": "Закрепить водителя за машиной, "
-            #     "необходимо помнить что за одной машиной не должно быть закреплено более 3х водителей.",
-            # },
         }
 
     async def add_driver(self, data: AddDriverSchema) -> FullDataDriverSchema:
@@ -58,9 +49,3 @@ class ManagerViews(BaseView):
         return FullDataCarSchema(
             car=car.as_dict, work_schedule_history=work_schedule_history.as_dict
         )
-
-    # async def create_assign_car_driver(
-    #     self, data: AssignCarDriverCreateSchema
-    # ) -> AssignCarDriverSchema:
-    #     result = await self.db.assign_car_driver(**data.model_dump())
-    #     return result
