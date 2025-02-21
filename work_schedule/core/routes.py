ea13ec783cf4f1219c1a7c7be2f788c8f058ab59
vivera83 <vivera83@yandex.ru>
2025-02-21 09:37:51 +0300

    сняты коммиты

--- work_schedule/core/routes.py
+++ work_schedule/core/routes.py
@@ -9,9 +9,9 @@ from fastapi import FastAPI
 
 def setup_routes(app: FastAPI):
     """Настройка Роутов приложения."""
-    # app.include_router(CarViews(prefix="/car", tags=["CAR"]))
-    # app.include_router(DriverViews(prefix="/driver", tags=["DRIVER"]))
-    # app.include_router(WorkScheduleHistoryViews(prefix="/work_schedule", tags=["WORK SCHEDULE HISTORY"]))
+    app.include_router(CarViews(prefix="/car", tags=["CAR"]))
+    app.include_router(DriverViews(prefix="/driver", tags=["DRIVER"]))
+    app.include_router(WorkScheduleHistoryViews(prefix="/work_schedule", tags=["WORK SCHEDULE HISTORY"]))
     app.include_router(ManagerViews(prefix="/resources", tags=["АКТИВЫ"]))
     app.include_router(ScheduleType(prefix="/schedule_type", tags=["SCHEDULE TYPE"]))
 
